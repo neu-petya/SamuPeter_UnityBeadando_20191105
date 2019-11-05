25a61c435030656e518d5dcd7d26a8adc1ca894c
neu-petya <56402786+neu-petya@users.noreply.github.com>
2019-11-05 22:53:18 +0100

    feltöltés
    Bayat Games pixelart asset

---
+++ Assets/CoinPicked.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicked : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("collision.gameObject.name: " + gameObject.name);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Collide");
+        //Debug.Log("collision.gameObject.name: " + collision.gameObject.name);
+        //Debug.Log("gameObject.name: " + gameObject.name);
+        if (collision.gameObject.name == "Body")
+        {
+            //Destroy(gameObject);
+            CreateNewCoin();
+            //Begin();
+        }
+    }
+
+    void CreateNewCoin()
+    {
+        StartCoroutine(Begin());
+    }
+
+    IEnumerator Begin()
+    {
+        float xRandomTime = Random.Range(1, 5);
+        var scale = transform.localScale;
+        transform.localScale = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(xRandomTime);
+        transform.localScale = scale;
+        transform.position = new Vector3(Random.Range(-17, 17), 8, 0);
+    }
+}
---
+++ Assets/MoveMe.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour
+{
+    public float speed = 5f;
+    void Start()
+    {
+        Debug.Log("gameObject.name: " + gameObject.name);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collide");
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < -17)
+        {
+            transform.position = transform.position + new Vector3(Mathf.Abs(horizontalInput * speed * Time.deltaTime), 0, 0);
+        }
+        else if (transform.position.x > 17)
+        {
+            //transform.position = transform.position + new Vector3(((horizontalInput * speed * Time.deltaTime) - (horizontalInput * speed * Time.deltaTime)), 0, 0);
+            //transform.position = transform.position - new Vector3((horizontalInput * speed * Time.deltaTime), 0, 0);
+            transform.position = transform.position - new Vector3(Mathf.Abs(horizontalInput * speed * Time.deltaTime), 0, 0);
+        }
+        else
+        {
+            transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+        }
+
+        //if (transform.position.x > 17)
+        //{
+        //    transform.position = transform.position + new Vector3(((horizontalInput * movementSpeed * Time.deltaTime) - (horizontalInput * movementSpeed * Time.deltaTime)), 0, 0);
+        //}
+        //if (transform.position.x > -17)
+        //{
+        //    transform.position = transform.position + new Vector3((Math.Abs(horizontalInput * movementSpeed * Time.deltaTime), 0, 0));
+        //}
+        //else
+        //{
+        //    horizontalInput* speed * Time.deltaTime;
+        //}
+
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);*/
+    }
+}
